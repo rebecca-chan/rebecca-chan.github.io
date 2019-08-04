9f0fc83a49c6ae6ac7633b157fef9dfdc3cfc863
Rebecca Chan <chan.rebecca@gmail.com>
2019-08-04 10:14:38 -0400

    updated about

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,10 +14,10 @@ const About = () => (
       <div className="right">
         <h1>About Me</h1>
         <p>{`Hi! I'm Rebecca. I'm a software engineer living in NYC.`}</p>
-        <p>{`Currently I'm at Annalect, the adtech arm of Omnicom, working on an application that uses the Linkedin Audience API to build out professional audience profiles for Omnicom clients. I'm having lots of fun playing with data viz and S3.`}</p>
-        <p>{`In my free time, I enjoy indoor and outdoor cycling, making sauces, and spoiling my fur baby Terra, a sato from Puerto Rico.`}</p>
+        <p>{`Currently I'm at Annalect, the data science/adtech arm of Omnicom, working on an application that uses the Linkedin Audience API to build out professional audience profiles for Omnicom clients. It's built on D3, AngularJS, Flask, and S3.`}</p>
+        <p>{`In my free time, I enjoy indoor and outdoor cycling, making sauces, and spoiling my dog.`}</p>
         <p>
-          {`Fun fact: I once won a cycling contest for farthest distance biked in 1 minute`}
+          {`Fun fact: I once won a cycling contest for farthest distance biked in 1 minute and got to fly to Amsterdam for free.`}
         </p>
 
         <Link to="/">{`<-Back`}</Link>
